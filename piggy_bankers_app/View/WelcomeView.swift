96dfca131db88e89b77fdac989d9088d778ae6dd
Mitch Rogers <mitchellmel@outlook.com>
2023-01-20 11:57:51 -0600

    rebase

--- piggy_bankers_app/View/WelcomeView.swift
+++ piggy_bankers_app/View/WelcomeView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WelcomeView: View {
     @StateObject var userManager = UserManager()
     
     var body: some View {
@@ -58,8 +58,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WelcomeView()
     }
 }
